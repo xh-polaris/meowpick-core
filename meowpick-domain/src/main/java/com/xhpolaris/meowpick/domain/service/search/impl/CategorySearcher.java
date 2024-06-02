75538c2dd2a112059d54885b8a25df9c30ffb8c2
eicen <lc024437@gmail.com>
2024-06-02 10:13:29 +0800

    feat(课程搜索)

---
+++ meowpick-domain/src/main/java/com/xhpolaris/meowpick/domain/service/search/impl/CategorySearcher.java
@@ -0,0 +1,20 @@
+package com.xhpolaris.meowpick.domain.service.search.impl;
+
+import com.xhpolaris.meowpick.common.PageEntity;
+import com.xhpolaris.meowpick.common.enums.TypeEn;
+import com.xhpolaris.meowpick.domain.model.valobj.SearchCmd;
+import com.xhpolaris.meowpick.domain.service.CourseServer;
+import com.xhpolaris.meowpick.domain.service.search.AbstractSearcher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component(TypeEn.Name.category)
+public class CategorySearcher extends AbstractSearcher {
+    private final CourseServer server;
+
+    @Override
+    public PageEntity<?> search(SearchCmd.Query query) {
+        return server.search(query);
+    }
+}
